376cbeffe9359114af76f719055caaedf93bba1e
Vladislave1515 <vip.vlad.ego@mail.ru>
2024-12-12 16:47:17 +0700

    lesson_03

---
+++ lesson_03/lesson_3_task_1.py
@@ -0,0 +1,8 @@
+from user import User
+
+
+my_user = User("Владислав", "Ананьев")
+
+print(my_user.get_f_name())
+print(my_user.get_l_name())
+print(my_user.get_f_l_name())
---
+++ lesson_03/smartphone.py
@@ -0,0 +1,5 @@
+class Smartphone:
+    def __init__(self, brand, model, number):
+        self.smph_brand = brand
+        self.smph_model = model
+        self.smph_number = number
