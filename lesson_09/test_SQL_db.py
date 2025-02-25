715af75cc5a2d4908c60c6fcb736067b3553abc6
Vladislave1515 <vip.vlad.ego@mail.ru>
2025-02-25 13:18:23 +0700

    lesson09_1

--- lesson_09/test_SQL_db.py
+++ lesson_09/test_SQL_db.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine, text
+import pytest
 
 
 db_connection_string = "postgresql://postgres:1234@localhost:5432/QA"
 db = create_engine(db_connection_string)
 
 
-def add_student():
+@pytest.mark.run(order=1)
+def test_add_student():
     sql = text(
         "INSERT INTO student (user_id, level, education_form, subject_id) "
         "VALUES (:user_id, :level, :education_form, :subject_id)"
@@ -21,8 +23,19 @@ def add_student():
         )
         connection.commit()
 
+    check_sql = text("SELECT * FROM student")
+    with db.connect() as connection:
+        result = connection.execute(check_sql).fetchall()
+        last_entry = result[-1] if result else None
+        assert last_entry is not None
+        assert last_entry[0] == 1001
+        assert last_entry[1] == 'Bachelor'
+        assert last_entry[2] == 'Full-time'
+        assert last_entry[3] == 10
 
-def update_student():
+
+@pytest.mark.run(order=1)
+def test_update_student():
     sql = text(
         "UPDATE student SET level = :level, education_form = :education_form, "
         "subject_id = :subject_id WHERE user_id = :user_id"
@@ -38,8 +51,17 @@ def update_student():
         )
         connection.commit()
 
+    check_sql = text("SELECT * FROM student WHERE user_id = :user_id")
+    with db.connect() as connection:
+        result = connection.execute(check_sql, {'user_id': 1001}).fetchone()
+        assert result is not None
+        assert result[1] == 'Master'
+        assert result[2] == 'Part-time'
+        assert result[3] == 20
 
-def delete_student():
+
+@pytest.mark.run(order=1)
+def test_delete_student():
     sql = text(
         "DELETE FROM student WHERE user_id = :user_id"
     )
@@ -49,7 +71,7 @@ def delete_student():
         )
         connection.commit()
 
-
-add_student()
-update_student()
-delete_student()
+    check_sql = text("SELECT * FROM student WHERE user_id = :user_id")
+    with db.connect() as connection:
+        result = connection.execute(check_sql, {'user_id': 1001}).fetchone()
+        assert result is None
